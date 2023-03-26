dac74b7b93e0fbc86e5252267872158ec8d39585
kir563 <k563ke70@gmail.com>
2023-03-26 19:13:26 +0700

    исправил

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,51 +5,84 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
-    this.render(this.products)
+    this.render()
   }
 
-  render(products) {
+  render() {
     this.elem = createElement(`
     <div class="products-grid">
       <div class="products-grid__inner"></div>
     </div>
     `)
-    const productGrid = this.elem.querySelector('.products-grid__inner')
-    products.forEach(card => {
-      const productCard = new ProductCard(card).elem
-      productGrid.append(productCard)
-    })
-    this.elem.append(productGrid)
+    this.renderContent(this.products)
   }
 
-  updateFilter = (filters) => {
-    
-    this.filters  = Object.assign(this.filters, filters)
-    
-    let result = [...this.products]
+  renderContent(products) {
+    const inner = this.elem.querySelector('.products-grid__inner')
+    inner.innerHTML = '';
+    for (const product of products) {
+      if (this.filters.noNuts && product.nuts) {continue}
+      if (this.filters.vegeterianOnly && !product.vegeterian) {continue}
+      if (this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness) {continue}
+      if (this.filters.category && product.category !== this.filters.category) {continue}
 
-    if (this.filters['noNuts']){
-      result = result.filter(item => !item['nuts'])
-    }
-
-    if (this.filters['vegeterianOnly']){
-      result = result.filter(item => item['name'].includes('veg') || item['vegeterian'])
-    }
-
-    if (this.filters['maxSpiciness'] >= 0) {
-      result = result.filter(item => item['spiciness'] <= this.filters['maxSpiciness'])
-    }
-
-    if (this.filters['category']) {
-      result = result.filter(item => item['category'] === this.filters['category'])
-    }
-
-    const container = document.querySelector('#container')
-    
-    if (container) {
-      this.elem.remove()
-      this.render(result)
-      container.append(this.elem)
+      const card = new ProductCard(product).elem
+      inner.append(card)
     }
   }
+
+  updateFilter(filters) {
+    this.filters = Object.assign(this.filters, filters)
+    this.renderContent(this.products)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // updateFilter = (filters) => {
+    
+  //   this.filters  = Object.assign(this.filters, filters)
+    
+  //   let result = [...this.products]
+
+  //   if (this.filters['noNuts']){
+  //     result = result.filter(item => !item['nuts'])
+  //   }
+
+  //   if (this.filters['vegeterianOnly']){
+  //     result = result.filter(item => item['name'].includes('veg') || item['vegeterian'])
+  //   }
+
+  //   if (this.filters['maxSpiciness'] >= 0) {
+  //     result = result.filter(item => item['spiciness'] <= this.filters['maxSpiciness'])
+  //   }
+
+  //   if (this.filters['category']) {
+  //     result = result.filter(item => item['category'] === this.filters['category'])
+  //   }
+
+  //   const container = document.querySelector('#container')
+    
+  //   if (container) {
+  //     this.elem.remove()
+  //     this.render(result)
+  //     container.append(this.elem)
+  //   }
+  // }
 }
