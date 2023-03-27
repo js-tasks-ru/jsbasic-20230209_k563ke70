ad38fda995edca536d0d28726e094a85381b4d3d
kir563 <k563ke70@gmail.com>
2023-03-27 18:13:13 +0700

    added class Cart

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,28 +6,44 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {return}
+
+    const index = this.cartItems.findIndex(item => item.product.id === product.id)
+    if(index !== -1) {
+      this.cartItems[index].count++
+      this.onProductUpdate(this.cartItems[index])
+    } else {
+      this.cartItems.push({product: product, count: 1})
+      this.onProductUpdate(this.cartItems.at(-1))
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const index = this.cartItems.findIndex(item => item.product.id === productId)
+    if(index === -1) {return}
+    
+    const product = this.cartItems[index]
+    product.count += amount
+    if (product.count === 0) {
+      this.cartItems.splice(index, 1)
+    }
+    this.onProductUpdate(this.cartItems[index])
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems[0] ? false : true
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => acc + item.count, 0)
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => acc + item.product.price * item.count, 0)
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
